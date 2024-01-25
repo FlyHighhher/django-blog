55a8180d169f3aff1c8011c33deafa228d798aaf
Andres Salumets <andres.workmail@gmail.com>
2024-01-25 17:43:14 +0000

    Add changes to migrations and models

--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,7 @@ class Post(models.Model):
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+
 class Comment(models.Model):
     post = models.ForeignKey(
         Post,
